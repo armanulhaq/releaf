3a435e9ee8dbb58172a1b1a4e763a5cb6a2acaab
Arman Ul Haq <armanulhaq10@gmail.com>
2025-07-29 16:22:44 +0530

    fix issue where i forgot to add app.listen

--- server/index.js
+++ server/index.js
@@ -47,5 +47,8 @@ app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
 
-// Export for Vercel
-export default app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
